dab20225f99f43f6e5472cc19dcb7bc9ff9b4c2a
bx0518 <bx0518@gmail.com>
2026-03-17 21:12:09 +0800

    Persisting Auth State

--- lib/init_dependencies.dart
+++ lib/init_dependencies.dart
@@ -2,6 +2,7 @@ import 'package:blog_app/core/secrets/app_secrets.dart';
 import 'package:blog_app/features/auth/data/datasources/auth_remote_data_source.dart';
 import 'package:blog_app/features/auth/data/repositories/auth_repository_impl.dart';
 import 'package:blog_app/features/auth/domain/repository/auth_repository.dart';
+import 'package:blog_app/features/auth/domain/usecases/current_user.dart';
 import 'package:blog_app/features/auth/domain/usecases/user_login.dart';
 import 'package:blog_app/features/auth/domain/usecases/user_sign_up.dart';
 import 'package:blog_app/features/auth/presentation/bloc/auth_bloc.dart';
@@ -35,7 +36,15 @@ void _initAuth() {
 
   serviceLocator.registerFactory<UserLogin>(() => UserLogin(serviceLocator()));
 
+  serviceLocator.registerFactory<CurrentUser>(
+    () => CurrentUser(serviceLocator()),
+  );
+
   serviceLocator.registerLazySingleton<AuthBloc>(
-    () => AuthBloc(userSignUp: serviceLocator(), userLogin: serviceLocator()),
+    () => AuthBloc(
+      userSignUp: serviceLocator(),
+      userLogin: serviceLocator(),
+      currentUser: serviceLocator(),
+    ),
   );
 }
