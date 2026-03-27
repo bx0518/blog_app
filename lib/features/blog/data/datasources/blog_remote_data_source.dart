20523097bc72d9eccdc207dd3da64f1bea1d3dd0
bx0518 <bx0518@gmail.com>
2026-03-27 12:48:37 +0800

    Blog Upload - Dependencies Injection with GetIt, remove wrong "required"

--- lib/features/blog/data/datasources/blog_remote_data_source.dart
+++ lib/features/blog/data/datasources/blog_remote_data_source.dart
@@ -14,7 +14,7 @@ abstract interface class BlogRemoteDataSource {
 
 class BlogRemoteDataSourceImpl implements BlogRemoteDataSource {
   final SupabaseClient supabaseClient;
-  BlogRemoteDataSourceImpl({required this.supabaseClient});
+  BlogRemoteDataSourceImpl(this.supabaseClient);
 
   @override
   Future<BlogModel> uploadBlog(BlogModel blog) async {
